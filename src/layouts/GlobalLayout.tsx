71b58e15dabfbd3647bdb57220a9cfd901060362
Jarrett Lyon <64499632+jlyon12@users.noreply.github.com>
2024-01-27 18:54:18 -0700

    fix background image position

--- src/layouts/GlobalLayout.tsx
+++ src/layouts/GlobalLayout.tsx
@@ -5,7 +5,7 @@ const GlobalLayout = ({ children }: React.PropsWithChildren) => {
 	const { darkMode } = useDarkModeContext();
 	return (
 		<div className={darkMode ? 'dark' : ''}>
-			<div className="min-h-dvh bg-d-neutral-100 bg-mobile-light bg-cover bg-center bg-no-repeat px-6 pt-4 dark:bg-d-neutral-500 dark:bg-mobile-dark md:bg-tablet-light md:px-16 md:pt-12 dark:md:bg-tablet-dark lg:bg-desktop-light  dark:lg:bg-desktop-dark">
+			<div className="min-h-dvh bg-d-neutral-100 bg-mobile-light  bg-no-repeat px-6 pt-4 dark:bg-d-neutral-500 dark:bg-mobile-dark md:bg-tablet-light  md:px-16 md:pt-12 dark:md:bg-tablet-dark lg:bg-desktop-light dark:lg:bg-desktop-dark  xl:bg-cover  xl:bg-center">
 				<Header />
 				<main>{children}</main>
 				<footer></footer>
